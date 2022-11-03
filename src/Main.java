e84301a6f2e1cb692c4f5ecbcc8bb2ef6cf701cd
HP <mdzaid1299@gmail.com>
2022-11-04 02:13:23 +0530

    ini

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Enter length of array ");
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        int[] arr = new int[length];
+        int sum = 0;
+        System.out.println("Enter, the, element");
+        for (int i = 0; i < length; i++) {
+            arr[i] = scanner.nextInt();
+            sum = sum + arr[i];
+        }
+        System.out.println("sum = " + sum);
     }
 }
